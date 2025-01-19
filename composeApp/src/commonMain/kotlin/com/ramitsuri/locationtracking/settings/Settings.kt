3c11d9daa5dba510d29ce1c89f0d262e62ce8a1b
Ramit Suri <ramitsuri007@gmail.com>
2025-01-18 22:15:30 -0500

    Save next monitoring mode

--- composeApp/src/commonMain/kotlin/com/ramitsuri/locationtracking/settings/Settings.kt
+++ composeApp/src/commonMain/kotlin/com/ramitsuri/locationtracking/settings/Settings.kt
@@ -3,6 +3,7 @@ package com.ramitsuri.locationtracking.settings
 import com.ramitsuri.locationtracking.data.toEnum
 import com.ramitsuri.locationtracking.model.MonitoringMode
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 
 class Settings internal constructor(private val keyValueStore: KeyValueStore) {
@@ -18,7 +19,8 @@ class Settings internal constructor(private val keyValueStore: KeyValueStore) {
             }
     }
 
-    suspend fun setMonitoringMode(monitoringMode: MonitoringMode) {
+    suspend fun setNextMonitoringMode() {
+        val monitoringMode = getMonitoringMode().first().getNextMode()
         keyValueStore.putString(Key.MONITORING_MODE, monitoringMode.value)
     }
 }
